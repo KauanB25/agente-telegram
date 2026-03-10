a297d63191fa4eafa3f5dfcca1fe0331c691e9c6
Kauan Monteiro <kaauan748@gmail.com>
2026-03-10 19:30:16 -0300

    chore: clean up whitespaces

--- src/agente_telegram/config/settings.py
+++ src/agente_telegram/config/settings.py
@@ -8,32 +8,32 @@ class Settings(BaseSettings):
         alias='TOKEN_TELEGRAM',
         description="Token para comunicação com o telegram"
         )
-    
+
     db_postgree_host : str = Field(
-        default="localhost", 
+        default="localhost",
         description="Servidor que hospeda o banco de dados"
         )
-    
+
     db_postgree_port: int = Field(
-        default='5432', 
+        default='5432',
         description="porta do banco",
         alias="DB_POSTGREE_PORT"
         )
-    
+
     db_postgree_user: str =  Field(
         ...,
         description="Usuário para conexão",
         alias="DB_POSTGREE_USER"
         )
-    
+
     db_postgree_database: str = Field(
-        ..., 
+        ...,
         description="Nome do banco",
         alias="DB_POSTGREE_DATABASE"
         )
-    
+
     db_postgree_secret: SecretStr = Field(
-        ..., 
+        ...,
         description="Senha do banco",
         alias="DB_POSTGREE_PASSWORD"
         )
@@ -42,7 +42,7 @@ class Settings(BaseSettings):
         default="postgresql",
         description="Driver de comunicação"
         )
-    
+
     alembic_table_version: str = Field(
         default="alembic_version",
         description="Nome da tabela que armazena a versão atual do banco"
