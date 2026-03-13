037e61bbabea013144fa07cf26de36cf6713e37b
Kauan Monteiro <kaauan748@gmail.com>
2026-03-13 13:27:12 -0300

    refactor: convert functions to class methods

--- src/agente_telegram/bot.py
+++ src/agente_telegram/bot.py
@@ -15,86 +15,95 @@ logging.basicConfig(
 
 bot = telebot.TeleBot(settings.token_telegram.get_secret_value())
 
-start_chat = None
+class bot_telegram:
 
+    def __init__(self):
+        self.start_chat = None
 
-def cadastro_usuario(message: Message):
-    user_telegram = UserTelegram()
+    def cadastro_usuario(self, message: Message):
+        user_telegram = UserTelegram()
 
-    full_name = message.from_user.full_name
-
-    id_telegram = message.from_user.id
-
-    try:
-        user_telegram.consulta_existencia_usuario(id_telegram)
-
-    except ValueError:
-        logging.error(f'Usuário {id_telegram} não encontrado')
-
-        logging.info("Iniciando o cadastro do usuário")
-
-        confirmacao = user_telegram.insert_new_user(id_telegram, full_name)
-
-        if confirmacao:
-            logging.info("Cadastro realizado com sucesso")
-        else:
-            logging.error("Erro ao cadastrar o usuário")
-
-
-@bot.message_handler(commands=['start', 'hello'])
-def welcome(message: Message):
-    markup = ReplyKeyboardMarkup(
-        one_time_keyboard=True,
-        resize_keyboard=True
-        )
-
-    botao_contato = KeyboardButton(
-        "Você aceita compartilhar o seu telefone?",
-        request_contact=True)
-
-    markup.add(botao_contato)
-
-    cadastro_usuario(message)
-
-    bot.send_message(
-        message.chat.id,
-        "Olá, para continuar faça a confirmação abaixo:",
-        reply_markup=markup
-    )
-
-
-@bot.message_handler(content_types=['contact'])
-def handle_contact(message: Message):
-    if message.contact is not None:
-        phone = message.contact.phone_number
+        full_name = message.from_user.full_name
 
         id_telegram = message.from_user.id
 
+        try:
+            user_telegram.consulta_existencia_usuario(id_telegram)
+
+        except ValueError:
+            logging.error(f'Usuário {id_telegram} não encontrado')
+
+            logging.info("Iniciando o cadastro do usuário")
+
+            confirmacao = user_telegram.insert_new_user(id_telegram, full_name)
+
+            if confirmacao:
+                logging.info("Cadastro realizado com sucesso")
+            else:
+                logging.error("Erro ao cadastrar o usuário")
+
+
+    def welcome(self, message: Message):
+        markup = ReplyKeyboardMarkup(
+            one_time_keyboard=True,
+            resize_keyboard=True
+            )
+
+        botao_contato = KeyboardButton(
+            "Você aceita compartilhar o seu telefone?",
+            request_contact=True)
+
+        markup.add(botao_contato)
+
+        self.cadastro_usuario(message)
+
+        bot.send_message(
+            message.chat.id,
+            "Olá, para continuar faça a confirmação abaixo:",
+            reply_markup=markup
+        )
+
+
+    def handle_contact(self, message: Message):
+        if message.contact is not None:
+            phone = message.contact.phone_number
+
+            id_telegram = message.from_user.id
+
+            user_telegram = UserTelegram()
+
+            confirmacao = user_telegram.update_user_phone(id_telegram, phone)
+
+            if confirmacao:
+                logging.info("Atualização realizada com sucesso")
+            else:
+                logging.error("Erro ao atualizar o usuário")
+
+
+    def echo(self, message: Message):
         user_telegram = UserTelegram()
 
-        confirmacao = user_telegram.update_user_phone(id_telegram, phone)
+        self.cadastro_usuario(message)
 
-        if confirmacao:
-            logging.info("Atualização realizada com sucesso")
-        else:
-            logging.error("Erro ao atualizar o usuário")
+        number = user_telegram.consulta_phone_number(message.from_user.id)
 
+        if number:
+            if self.start_chat is None:
+                chat = GoogleChat()
+                logging.info("Chat iniciado")
+                self.start_chat = True
+            resposta = chat.send_message(message.text)
+            bot.reply_to(message, resposta)
+            return
 
-@bot.message_handler(func=lambda msg: True)
-def echo(message: Message):
-    user_telegram = UserTelegram()
+        bot.reply_to(message, "Digite /start e confirme seu número para continuar")
 
-    cadastro_usuario(message)
+    def start_bot(self):
+        bot.infinity_polling()
 
-    number = user_telegram.consulta_phone_number(message.from_user.id)
-
-    if number:
-        chat = GoogleChat()
-        resposta = chat.send_message(message.text)
-        bot.reply_to(message, resposta)
-        return
-
-    bot.reply_to(message, "Digite /start e confirme seu número para continuar")
-
-
-bot.infinity_polling()
+if __name__ == "__main__":
+    instancia = bot_telegram()
+    bot.message_handler(commands=['start', 'hello'])(instancia.welcome)
+    bot.message_handler(content_types=['contact'])(instancia.handle_contact)
+    bot.message_handler(func=lambda msg: True)(instancia.echo)
+    instancia.start_bot()
