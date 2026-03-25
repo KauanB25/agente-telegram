ef668df822d28fcd20acbadc828b6c967e0b8719
Kauan Bezerra <kaauan748@gmail.com>
2026-03-25 12:08:00 -0300

    chore: set webhook url in script

---
+++ scripts/set_webhook.py
@@ -0,0 +1,17 @@
+import telebot
+from agente_telegram.config.settings import settings
+
+bot = telebot.TeleBot(settings.token_telegram.get_secret_value())
+
+
+bot.remove_webhook()
+
+url_webhook = settings.url_webhook
+
+# Define o novo webhook
+sucesso = bot.set_webhook(url=url_webhook)
+
+if sucesso:
+    print(f"✅ Webhook configurado com sucesso para: {url_webhook}")
+else:
+    print("❌ Falha ao configurar o webhook.")
